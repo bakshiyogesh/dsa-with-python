44baeec5e86dddc71ecbad8949f8da98f49f2b3d
bakshiyogesh <yogeshbakshi589@gmail.com>
2026-03-24 02:54:11 -0500

    feat:added double linkedlist,singly linkedlist

---
+++ double-linked-list.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class DoubleLinkedList:
+    def __init__(self, head=None, next=None):
+        self.head = head
+        self.next = next
+     
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+        
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        current = self.head
+        if not self.head:
+            self.head = new_node
+            return
+        while current.next:
+            current = current.next
+        current.next = new_node
+        new_node.prev = current
+    
+    def insert_at_middle(self,data,value):
+        new_node=Node(data)
+        current=self.head
+        if not self.head:
+            self.head = new_node
+            return
+        while current:
+            if current.data == value:
+                new_node.next = current.next
+                new_node.prev = current
+                current.next = new_node
+                new_node.next.prev = new_node
+                return
+            current = current.next
+    def delete(self, value):
+        current = self.head
+        if not current:
+            return
+        if current.data == value:
+            self.head = current.next
+            return
+        while current:
+            if current.data == value:
+                if not current.next:
+                    current.prev.next = None
+                    return
+                else:
+                    current.prev.next = current.next
+                    current.next.prev = current.prev
+                    return
+            current = current.next
+        
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+
+double_list = DoubleLinkedList()
+double_list.insert_at_beginning(1)
+double_list.insert_at_beginning(2)
+double_list.insert_at_beginning(3)
+double_list.insert_at_middle(4, 2)
+double_list.print_list()
+double_list.delete(2)
+double_list.print_list()
---
+++ single-linked-List.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, data=None,next=None):
+        self.data = data
+        self.next = next
+
+class SingleLinkedList:
+    def __init__(self,head=None):
+        self.head = head
+    
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+         
+    def insert_at_end(self,data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+
+    def insert_at_middle(self, data,value):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        else:
+            current = self.head
+            while current.next:
+                if current.data == value:
+                    new_node.next = current.next
+                    current.next = new_node
+                    break
+                current = current.next
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print(current)
+
+    
+    def delete(self, data):
+        current = self.head
+        prev=None
+        if not current:
+            return
+        if current.data == data:
+            # prev = None
+            self.head = current.next 
+            # current = None
+            return
+        while current.next:
+            if current.data == data:
+                prev.next = current.next
+                # current.next = current.next.next 
+                return 
+                # break
+            prev = current
+            current = current.next
+        if current.data == data:
+            prev.next = None
+    
+
+# Example usage:
+if __name__ == "__main__":
+    ll = SingleLinkedList()
+    ll.insert_at_end(1)
+    ll.insert_at_end(2)
+    ll.insert_at_end(3)
+    ll.insert_at_end(5)
+    ll.print_list()
+    ll.delete(2)
+    ll.print_list()
+
+
+# new_node is temp variable
